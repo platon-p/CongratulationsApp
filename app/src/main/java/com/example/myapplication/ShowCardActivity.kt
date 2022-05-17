c30cf59cfaeb21f0ae3310cad90316e5fae020c4
platon-p <platonpechenev@ya.ru>
2022-05-17 22:35:21 +0300

    init fields in show card activity

--- app/src/main/java/com/example/myapplication/ShowCardActivity.kt
+++ app/src/main/java/com/example/myapplication/ShowCardActivity.kt
@@ -2,43 +2,73 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.database.AppDatabase
 import io.reactivex.android.schedulers.AndroidSchedulers
 
 class ShowCardActivity : AppCompatActivity() {
-    lateinit var titleTextView: TextView
+    private lateinit var titleTextView: TextView
+    private lateinit var cardNameInput: EditText
+    private lateinit var userNameInput: EditText
+    private lateinit var deleteButton: Button
+    private lateinit var saveButton: Button
+    private lateinit var shareButton: Button
+
     private var cardId: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_card)
 
-        titleTextView = findViewById(R.id.showCardTitle)
         cardId = intent.getIntExtra("CardId", 0).toLong()
-        loadTitle()
+
+
+        titleTextView = findViewById(R.id.showCardTitle)
+
+        cardNameInput = findViewById(R.id.editTextCardName)
+        userNameInput = findViewById(R.id.editTextPersonName)
+
+        deleteButton = findViewById(R.id.deleteButton)
+        deleteButton.setOnClickListener { delete() }
+
+        saveButton = findViewById(R.id.saveButton)
+        saveButton.setOnClickListener { save() }
+
+        shareButton = findViewById(R.id.shareButton)
+        shareButton.setOnClickListener { share() }
+
+        initFields()
 
         // TODO: Add card form (fragment or layout)
     }
 
-    fun delete() {
+    private fun share() {
+        // TODO("Share pdf as file or as link")
+    }
+
+    private fun delete() {
         // TODO: Delete card by id
         // TODO: Delete file
     }
 
-    fun save() {
+    private fun save() {
         // TODO: validate fields & save
         // TODO: show toast if success
     }
 
     @SuppressLint("CheckResult")
-    private fun loadTitle() {
+    private fun initFields() {
         AppDatabase.getDatabase(applicationContext)
             .cardDao().getById(cardId)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 {
                     titleTextView.text = it.name
+                    cardNameInput.setText(it.name)
+                    userNameInput.setText(it.fio)
                 },
                 { finish() })
     }
